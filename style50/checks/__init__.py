b8021a34d3062d56b81493769f3aee152306c926
Chad Sharp <cmlsharp@umich.edu>
2017-08-06 14:08:18 -0400

    reorganize __init__.py

--- style50/checks/__init__.py
+++ style50/checks/__init__.py
@@ -1,9 +1,12 @@
-
-from os.path import dirname, basename, isfile
 import glob
-modules = glob.glob(dirname(__file__)+"/*.py")
-__all__ = ["extensions"] + [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+import os
 
 extensions = {}
+__all__ = [os.path.basename(f)[:-3]
+            for f in glob.glob(os.path.dirname(__file__) + "/*.py")
+            if os.path.isfile(f) and not f.endswith("__init__.py")] \
+          + ["extensions", "Error", "DependencyError"]
+
+
 from .base import Error, DependencyError
 from . import *
