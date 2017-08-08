b9aa517a7539a35339a37cc96af191f9d4d40738
Chad Sharp <cmlsharp@umich.edu>
2017-08-08 12:22:54 -0400

    Fix piped output bug

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "Topic :: Utilities"
     ],
     description="This is style50, with which you can check code against the CS50 style guide",
-    install_requires=["argparse", "autopep8", "backports.shutil_get_terminal_size", "icdiff", "jsbeautifier", "six", "termcolor"],
+    install_requires=["argparse", "autopep8", "icdiff", "jsbeautifier", "six", "termcolor"],
     dependency_links=["git+https://github.com/jeffkaufman/icdiff.git"],
     keywords=["style", "style50"],
     name="style50",
