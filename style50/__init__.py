7c09aa8a5662f71a8b182c7a622af05d14b1b681
Chad Sharp <chad@cs50.harvard.edu>
2017-08-14 09:36:52 -0400

    Fix commenting style

--- style50/__init__.py
+++ style50/__init__.py
@@ -4,11 +4,11 @@ import os
 # https://stackoverflow.com/questions/17583443/what-is-the-correct-way-to-share-package-version-with-setup-py-and-the-package
 try:
     _dist = get_distribution("style50")
-    # Normalize path for cross-OS compatibility
+    # Normalize path for cross-OS compatibility.
     _dist_loc = os.path.normcase(_dist.location)
     _here = os.path.normcase(__file__)
     if not _here.startswith(os.path.join(_dist_loc, "style50")):
-        # this version is not installed, but another version is.
+        # This version is not installed, but another version is.
         raise DistributionNotFound
 except DistributionNotFound:
     __version__ = "locally installed, no version information available"
@@ -19,5 +19,5 @@ __all__ = ["Style50", "languages", "StyleCheck", "Error"]
 
 from .style50 import Style50, StyleCheck, Error
 
-# Ensure that all language checks are registered
+# Ensure that all language checks are registered.
 from . import languages
