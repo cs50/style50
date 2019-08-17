72eca866421bd02679c0309d901ee15e3c51ee78
Chad Sharp <chad@cs50.harvard.edu>
2019-08-17 18:21:21 -0400

    version++

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setup(
         "console_scripts": ["style50=style50.__main__:main"],
     },
     url="https://github.com/cs50/style50",
-    version="2.7.2",
+    version="2.7.3",
     include_package_data=True,
 )
