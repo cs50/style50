ab427b30a2ddda63917869867863c0ee7022bbad
LittleLightLittleFire <littlelightlittlefire@gmail.com>
2026-03-03 11:07:19 +1100

    Fixes terminal size

--- style50/_api.py
+++ style50/_api.py
@@ -22,34 +22,7 @@ from . import __version__, renderer
 
 __all__ = ["Style50", "StyleCheck", "Error"]
 
-
-
-def get_terminal_size(fallback=(80, 24)):
-    """
-    Return tuple containing columns and rows of controlling terminal, trying harder
-    than shutil.get_terminal_size to find a tty before returning fallback.
-
-    Theoretically, stdout, stderr, and stdin could all be different ttys that could
-    cause us to get the wrong measurements (instead of using the fallback) but the much more
-    common case is that IO is piped.
-    """
-    for stream in [sys.__stdout__, sys.__stderr__, sys.__stdin__]:
-        try:
-            # Make WINSIZE call to terminal
-            data = fcntl.ioctl(stream.fileno(), TIOCGWINSZ, b"\x00\x00\00\x00")
-        except OSError:
-            pass
-        else:
-            # Unpack two shorts from ioctl call
-            lines, columns = struct.unpack("hh", data)
-            break
-    else:
-        columns, lines = fallback
-
-    return columns, lines
-
-
-COLUMNS, LINES = get_terminal_size()
+COLUMNS, LINES = os.get_terminal_size()
 
 
 class Style50:
